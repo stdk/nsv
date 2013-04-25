66a5602160eeac40257397ccceab8c763f957475
feanor <std.feanor@gmail.com>
2013-04-25 17:25:41 +0300

    Reimplemented adbk module to support larger event blocks. Fixed incorrect alignment in get_events and get_last_event handlers.

--- src/adbk.cpp
+++ src/adbk.cpp
@@ -14,21 +14,52 @@
 
 #include <event.h>
 
-#define EVENTS_TO_READ_COUNT 30
+#define EVENTS_TO_READ_COUNT 123
 
+#define VALID_CRC 0x1234
+
+const timeval ADBK_TIMEOUT = { 2, 0 };
+
+struct __attribute__((packed)) SCommandExt
+{
+  SCommand command;
+  uint8_t data[0];
+
+  void init(SCommand* command, void* blocks,size_t blocks_length) {
+      this->command = *command;
+      memcpy(this->data,blocks,blocks_length);
+  }
+};
+
+struct __attribute__((packed)) SAnswerExt
+{
+  SAnswer answer;
+  uint8_t data[0];
+};
+
+#define WRAP_LEN (sizeof(((SPacket*)0)->len) + sizeof(((SPacket*)0)->type) + sizeof(((SPacket*)0)->crc))
 struct __attribute__ ((packed)) SPacket
 {
     uint32_t len;
     uint8_t type;
-    void* data;
+    uint8_t data[8192];
     size_t datalen; //helper
+    size_t datalen_left; //helper
     uint16_t crc;
 
-#define WRAP_LEN (sizeof(((SPacket*)0)->len) + sizeof(((SPacket*)0)->type) + sizeof(((SPacket*)0)->crc))
+    int init(uint8_t packet_type,SCommand* command,void* blocks,size_t blocks_length) {
+        this->datalen = sizeof(*command) + blocks_length;
+        if(this->datalen > sizeof(this->data)) {
+            this->len = 0;
+            return -1;
+        }
 
-    static SPacket create(uint8_t packet_type,void* data,size_t datalen) {
-        SPacket packet = { datalen + WRAP_LEN,packet_type,data,datalen,0x1234 };
-        return packet;
+        SCommandExt* ext = (SCommandExt*)this->data;
+        ext->init(command,blocks,blocks_length);
+
+        this->len = this->datalen + WRAP_LEN;
+        this->type = packet_type;
+        this->crc = VALID_CRC;
     }
 
     int send(int fd) {
@@ -58,7 +89,7 @@ struct __attribute__ ((packed)) SPacket
         return 0;
     }
 
-    int recv(int fd,void* data,size_t full_datalen) {
+    int recv_header(int fd) {
         const int header_len = sizeof(len) + sizeof(type);
         ssize_t bytes = ::recv(fd,this,header_len,0);
         xlog("SPacket header recv[%i]",bytes);
@@ -66,44 +97,65 @@ struct __attribute__ ((packed)) SPacket
             xlog2("SPacket header recv: %s",strerror(errno));
             return -1;
         }
-        xlog("len[%u] type[%i]",len,type);
+        xlog("recv_header len[%u] type[%i]",len,type);
 
         datalen = len - WRAP_LEN;
-        xlog("SPacket data len[%i]",datalen);
-        if(datalen > full_datalen) {
-            xlog2("SPacket data recv: datalen > full_datalen[%i]",full_datalen);
+        datalen_left = datalen;
+        xlog("SPacket data len[%i] left[%i]",datalen,datalen_left);
+
+        if(datalen > sizeof(this->data)) {
+            xlog2("datalen[%i] > sizeof(this->data)",datalen);
             return -2;
-        }
-
-        bytes = ::recv(fd,data,datalen,0);
-        xlog("SPacket data recv[%i]",bytes);
-        if(bytes != (ssize_t)datalen) {
-            xlog2("SPacket data recv: %s",strerror(errno));
-            return -3;
-        }
-
-        bytes = ::recv(fd,&crc,sizeof(crc),0);
-        xlog("SPacket crc recv[%i]",bytes);
-        if(bytes != sizeof(crc)) {
-            xlog2("SPacket crc recv: %s",strerror(errno));
-            return -4;
-        }
-
-        if(crc != 0x1234) {
-            xlog2("SPacket crc recv: != 0x1234");
-            return -5;
         }
 
         return 0;
     }
+
+    int recv_data(int fd) {
+        uint8_t *data_current = data + datalen - datalen_left;
+        ssize_t bytes = ::recv(fd,data_current,datalen_left,0);
+        xlog("SPacket data recv[%i] out of[%i/%i]",bytes,datalen_left,datalen);
+        if(bytes < 0) {
+            xlog2("SPacket data recv: %s",strerror(errno));
+            return -3;
+        }
+
+        datalen_left -= bytes;
+        if(!datalen_left) {
+            bytes = ::recv(fd,&crc,sizeof(crc),0);
+            xlog("SPacket crc recv[%i]",bytes);
+            if(bytes != sizeof(crc)) {
+                xlog2("SPacket crc recv: %s",strerror(errno));
+                return -4;
+            }
+
+            if(crc != VALID_CRC) {
+                xlog2("SPacket crc recv: != %X",VALID_CRC);
+                return -5;
+            }
+
+            return 0;
+        }
+
+        return 1;
+    }
 };
 
-struct adbk_command
+struct IADBKCommandResult
 {
-    typedef void (*cmd_cb)(adbk_command*);
+    virtual uint32_t ip()=0;
+    virtual DC* dc()=0;
+    virtual device_data* device()=0;
+    virtual const SPacket* packet()=0;
+    virtual ~IADBKCommandResult() {}
+};
 
-    uint32_t ip;
-    DC *container;
+struct adbk_command : public IADBKCommandResult
+{
+    typedef void (*cmd_cb)(IADBKCommandResult*);
+
+    uint32_t m_ip;
+    DC *m_container;
     cmd_cb callback;
 
     int fd;
@@ -112,30 +164,40 @@ struct adbk_command
     timeval timeout;
     event timeout_ev;
 
-    char data[2048];
-    size_t len;
+    SPacket m_packet;
+
+    virtual uint32_t ip() {
+        return m_ip;
+    }
+
+    virtual DC* dc() {
+        return m_container;
+    }
+
+    virtual device_data* device() {
+        return m_container->deviceByAddr(m_ip);
+    }
+
+
+    virtual const SPacket* packet() {
+        return &m_packet;
+    }
 
     adbk_command(uint32_t ip,DC* container,SCommand* command,cmd_cb cb,
                  void* blocks=0,size_t blocks_length=0) {
         xlog("adbk_command[%i]",command->cmd);
-        this->ip = ip;
-        this->container = container;
+        this->m_ip = ip;
+        this->m_container = container;
         this->callback = cb;
 
-        memcpy(this->data,command,sizeof(*command));
-        set_data(blocks,blocks_length);
+        m_packet.init(ptCommand,command,blocks,blocks_length);
     }
 
-    ~adbk_command() {
+    virtual ~adbk_command() {
         xlog("~adbk_command");
         event_del(&socket_ev);
         event_del(&timeout_ev);
         close(fd);
-    }
-
-    void set_data(void* blocks,size_t length) {
-        memcpy(this->data + sizeof(SCommand),blocks,length);
-        this->len = sizeof(SCommand) + length;
     }
 
     static void timeout_callback(int fd,short event,void* arg) {
@@ -146,8 +208,7 @@ struct adbk_command
     void set_timeout() {
         xlog("adbk_command set_timeout");
 
-        timeout.tv_sec = 2;
-        timeout.tv_usec = 0;
+        timeout = ADBK_TIMEOUT;
 
         evtimer_set(&timeout_ev,timeout_callback,this);
         evtimer_add(&timeout_ev,&timeout);
@@ -160,20 +221,31 @@ struct adbk_command
 
 };
 
-static void adbk_recv_stage(int fd,short event,void *arg)
+static void adbk_recv_data_stage(int fd, short event,void *arg)
 {
-    xlog("adbk_recv_stage");
+    xlog("adbk_recv_data_stage");
     adbk_command* c = (adbk_command*)arg;
 
-    SPacket answer;
-    if( answer.recv(fd,c->data,sizeof(c->data)) < 0 ) {
-        xlog("adbk_recv_stage: failed");
+    int ret = c->m_packet.recv_data(fd);
+    if(ret == 1) {
+        c->next_stage(adbk_recv_data_stage,EV_READ);
     } else {
-        c->len = answer.datalen;
-        c->callback(c);
+        !ret ? c->callback(c) : xlog2("adbk_recv_stage: recv_data failed");
+        delete c;
+    }
+}
+
+static void adbk_recv_header_stage(int fd, short event,void *arg)
+{
+    xlog("adbk_recv_header_stage");
+    adbk_command* c = (adbk_command*)arg;
+
+    if( c->m_packet.recv_header(fd) < 0) {
+        xlog2("adbk_recv_stage: recv_header failed");
+        delete c;
     }
 
-    delete c;
+    c->next_stage(adbk_recv_data_stage,EV_READ);
 }
 
 static void adbk_send_stage(int fd,short event,void *arg)
@@ -181,19 +253,18 @@ static void adbk_send_stage(int fd,short event,void *arg)
     xlog("adbk_send_stage");
     adbk_command* c = (adbk_command*)arg;
 
-    int ret = SPacket::create(ptCommand,c->data,c->len).send(fd);
-    if(ret < 0) {
+    if(c->m_packet.send(fd) < 0) {
         xlog("adbk_send_stage: failed");
         delete c;
         return;
     }
 
-    c->next_stage(adbk_recv_stage,EV_READ);
+    c->next_stage(adbk_recv_header_stage,EV_READ);
 }
 
-static int send_adbk_command(adbk_command* c)
+static int send_adbk_command(adbk_command *c)
 {
-    c->fd = create_connection(c->ip,ADBK_PORT);
+    c->fd = create_connection(c->m_ip,ADBK_PORT);
     if(c->fd < 0) return -1;
 
     c->set_timeout();
@@ -202,17 +273,17 @@ static int send_adbk_command(adbk_command* c)
     return 0;
 }
 
-template<int command,void (*callback)(adbk_command* cmd)>
-static void execute(device_data* device,DC* container) {
+template<int command,void (*callback)(IADBKCommandResult *cmd)>
+static void execute(device_data *device,DC *container) {
     SCommand cmd = { command,0,0,btNone,0 };
     adbk_command* c = new adbk_command(device->addr,container,&cmd,callback);
     send_adbk_command(c);
 }
 
-template<void (*callback)(adbk_command* cmd),typename DataType>
-static void execute(device_data* device,DC* container,SCommand *cmd,DataType* data=(char*)0) {
-    adbk_command* c = new adbk_command(device->addr,container,cmd,callback);
-    if(data) c->set_data(data,sizeof(DataType));
+template<void (*callback)(IADBKCommandResult* cmd),typename DataType>
+static void execute(device_data* device,DC *container,SCommand *cmd,DataType *data=(char*)0) {
+    size_t data_length = data ? sizeof(DataType) : 0;
+    adbk_command *c = new adbk_command(device->addr,container,cmd,callback,data,data_length);
     send_adbk_command(c);
 }
 
@@ -222,62 +293,65 @@ class ADBKService : public IDeviceProcessor
 public:
     ADBKService(DC* con):container(con) {}
 
-    static void handle_sync_time(adbk_command* cmd) {
+    static void handle_sync_time(IADBKCommandResult* ret) {
         xlog("handle_sync_time");
+
+        const SPacket *packet = ret->packet();
         struct __attribute__ ((packed)) sync_time_answer {
             SAnswer s;
-        } *answer = (sync_time_answer*)cmd->data;
+        } *answer = (sync_time_answer*)packet->data;
 
         if(answer->s.err_code != answOK) {
             return xlog2("answer[%i] != answOk",answer->s.err_code);
         }
-        if(cmd->len < sizeof(*answer)) {
-            return xlog2("answer[%i] < required[%i]",cmd->len,sizeof(*answer));
+        if(packet->datalen < sizeof(*answer)) {
+            return xlog2("answer[%i] < required[%i]",packet->datalen,sizeof(*answer));
         }
 
-        if(device_data* device = cmd->container->deviceByAddr(cmd->ip)) {
-            //device->answer(device->addr,device->sn);
+        if(device_data* device = ret->device()) {
             device->inner_time = device->last_answer_time;
         } else {
-            xlog2("handle_sync_time: there is no device[%X]",cmd->ip);
+            xlog2("handle_sync_time: there is no device[%X] related to this command",ret->ip());
         }
     }
 
-    static void handle_get_state(adbk_command* cmd) {
+    static void handle_get_state(IADBKCommandResult* ret) {
         xlog("handle_get_state");
 
+        const SPacket *packet = ret->packet();
         struct __attribute__ ((packed)) get_state_answer {
             SAnswer s;
             SStateBlock state;
-        } *answer = (get_state_answer*)cmd->data;
+        } *answer = (get_state_answer*)packet->data;
 
         if(answer->s.err_code != answOK) {
             return xlog2("answer[%i] != answOk",answer->s.err_code);
         }
-        if(cmd->len < sizeof(*answer)) {
-            return xlog2("answer[%i] < required[%i]",cmd->len,sizeof(*answer));
+        if(packet->datalen < sizeof(*answer)) {
+            return xlog2("answer[%i] < required[%i]",packet->datalen,sizeof(*answer));
         }
 
-        if(device_data* device = cmd->container->deviceByAddr(cmd->ip)) {
+        if(device_data* device = ret->device()) {
            snprintf(device->version,sizeof(device->version),"%s.%s",answer->state.sw_ver,
                     answer->state.card.hw_ver+strlen(answer->state.card.hw_ver)-2);
 
            uint64_t sn = 0;
            memcpy(&sn,answer->state.card.sn,sizeof(sn));
 
-           cmd->container->answer(device->addr,sn);
+           ret->dc()->answer(device->addr,sn);
         } else {
-            xlog2("handle_get_state: there is no device[%X]",cmd->ip);
+            xlog2("handle_sync_time: there is no device[%X] related to this command",ret->ip());
         }
     }
 
-    static void handle_get_events(adbk_command* cmd) {
+    static void handle_get_events(IADBKCommandResult* ret) {
         xlog("handle_get_events");
 
-        struct get_events_answer {
+        const SPacket *packet = ret->packet();
+        struct __attribute__ ((packed)) get_events_answer {
             SAnswer s;
-            FLASHEVENT event;
-        } *answer = (get_events_answer*)cmd->data;
+            FLASHEVENT event[0];
+        } *answer = (get_events_answer*)packet->data;
 
         if(answer->s.err_code != answOK) {
             return xlog2("answer[%i] != answOk",answer->s.err_code);
@@ -288,37 +362,39 @@ public:
         if(answer->s.block_type != btEvent) {
             return xlog2("block[%i] != btEvent",answer->s.block_type);
         }
+        xlog("answer->s.block_count[%i]",answer->s.block_count);
         const size_t required_length = sizeof(SAnswer) + sizeof(FLASHEVENT)*answer->s.block_count;
-        if(cmd->len <  required_length) {
-            return xlog2("answer[%i] < required[%i]",cmd->len,required_length);
+        if(packet->datalen <  required_length) {
+            return xlog2("answer[%i] < required[%i]",packet->datalen,required_length);
         }
 
-        if(device_data *device = cmd->container->deviceByAddr(cmd->ip)) {
+        if(device_data *device = ret->device()) {
             for(int i=0;i< answer->s.block_count;i++) {
-                FLASHEVENT *f = &answer->event + i;
-                cmd->container->eventStorage()->save_event(device,f);
+                FLASHEVENT *f = answer->event + i;
+                ret->dc()->eventStorage()->save_event(device,f);
                 device->current_event_id = f->EventNumber;
             }
         } else {
-             xlog2("handle_get_events: there is no device[%X]",cmd->ip);
+             xlog2("handle_sync_time: there is no device[%X] related to this command",ret->ip());
         }
     }
 
-    static void handle_get_last_event(adbk_command* cmd) {
+    static void handle_get_last_event(IADBKCommandResult* ret) {
          xlog("handle_get_last_event");
 
-         struct get_last_event_answer {
+         const SPacket *packet = ret->packet();
+         struct __attribute__ ((packed)) get_last_event_answer {
              SAnswer s;
              FLASHEVENT event;
-         } *answer = (get_last_event_answer*)cmd->data;
+         } *answer = (get_last_event_answer*)packet->data;
          if(answer->s.err_code != answOK) {
              return xlog2("answer[%i] != answOk",answer->s.err_code);
          }
-         if(cmd->len < sizeof(*answer)) {
-             return xlog2("answer[%i] < required[%i]",cmd->len,sizeof(*answer));
+         if(packet->datalen < sizeof(*answer)) {
+             return xlog2("answer[%i] < required[%i]",packet->datalen,sizeof(*answer));
          }
 
-         if(device_data* device = cmd->container->deviceByAddr(cmd->ip)) {
+         if(device_data* device =  ret->device()) {
             device->last_event_id = answer->event.EventNumber;
 
             SCommand getEventsCommand = {
@@ -328,9 +404,10 @@ public:
                 btNone,
                 0
             };
-            execute<handle_get_events>(device,cmd->container,&getEventsCommand,(char*)0);
+
+            execute<handle_get_events>(device,ret->dc(),&getEventsCommand,(char*)0);
          } else {
-             xlog2("handle_get_last_event: there is no device[%X]",cmd->ip);
+             xlog2("handle_sync_time: there is no device[%X] related to this command",ret->ip());
          }
     }
 
@@ -345,23 +422,24 @@ public:
     }
 };
 
-void handle_get_update(adbk_command* cmd)
+void handle_get_update(IADBKCommandResult* ret)
 {
-     xlog2("handle_get_update");
+     xlog("handle_get_update");
 
-     SAnswer *answer = (SAnswer*)cmd->data;
+     const SPacket *packet = ret->packet();
+     SAnswer *answer = (SAnswer*)packet->data;
 
      if(answer->err_code != answOK) {
 
-         if(device_data* device = cmd->container->deviceByAddr(cmd->ip)) {
+         if(device_data* device = ret->device()) {
              device->progress = answer->err_code;
          }
 
          return xlog2("answer[%i] != answOk",answer->err_code);
      }
 
-     if(cmd->len < sizeof(*answer)) {
-         return xlog2("answer[%i] < required[%i]",cmd->len,sizeof(*answer));
+     if(packet->datalen < sizeof(*answer)) {
+         return xlog2("answer[%i] < required[%i]",packet->datalen,sizeof(*answer));
      }
 }
 
@@ -415,20 +493,6 @@ int adbk_update_cmd(char adbk_num,const char* filename,DC* container)
         if( md5_firmware_file(filename,block.md5,sizeof(block.md5)) < 0) {
             return -2;
         }
-
-        /*const char* firmware_dir = getenv("FIRMWARE_DIR");
-        char popen_cmd[200] = {0};
-        snprintf(popen_cmd,sizeof(popen_cmd),"/bin/md5sum %s/%s",firmware_dir,filename);
-
-        FILE* md5_file = popen(popen_cmd,"r");
-        if(!md5_file) {
-            xlog2("adbk_update_cmd popen[%s]",strerror(errno));
-            return -3;
-        }
-        fread(block.md5,32,1,md5_file);
-        fclose(md5_file);
-
-        xlog2("md5[%.*s]",32,block.md5);*/
 
         execute<handle_get_update>(device,container,&updateCmd,&block);
 
